9d0fe09b0a85f00773c50e5a9f9e37cbdb5e36d3
Drew Steen <andrew.decker.steen@gmail.com>
2021-01-05 13:09:38 -0500

    Added example simulation data and corresponding documentation.

---
+++ R/func_redun_sim.R
@@ -0,0 +1,7 @@
+##' Results of functional redundancy simulations
+##'
+##' Can put a paragraph of explanation here
+##'
+##' #' @format A data frame with four variables: \code{fr}, \code{method},
+#'   \code{loss.frac}, and \code{express.level}. Fill out the details about what each one means a bit.
+"func_redun_sim"
