2dbc1797d650f316b0172c65ae63da37f0834a4e
Drew Steen <andrew.decker.steen@gmail.com>
2021-02-03 12:55:19 -0500

    Taylor's version

--- R/func_redun_sim.R
+++ R/func_redun_sim.R
@@ -1,7 +1,80 @@
 ##' Results of functional redundancy simulations
-##'
-##' Can put a paragraph of explanation here
-##'
-##' #' @format A data frame with four variables: \code{fr}, \code{method},
-#'   \code{loss.frac}, and \code{express.level}. Fill out the details about what each one means a bit.
-"func_redun_sim"
+##' @export
+##' @description Performs extinction simulations and calculates functional redundancy using the Royalty method. 
+##' @param range.express The number of taxa contributing to a community-aggregated parameter--vector
+##' @param range.nonexpress The number of taxa not contributing to a community-aggregated parameter--vector
+##' @param q The diversity order to use when calculate functional redundancy--real number
+##' @param a.range A parameter modulating how even a lognormal abundance distribution is--vector
+##' @param loss.thres A parameter specifying the total loss trait in a community necessary to stop a simulation--number between 0 and 1
+##' @param range.trait Possible range of trait values to be assigned to taxa--a vector
+##' @param n.community Number of artificial communities to generate--integer greater than 0
+##' @param n.extinct.sim Number of extinction simulations to perform on every artificial community--integer greater than 0
+##' @details ANYTHING THAT NEEDS TO GO INTO THE DETAILS SECTION
+##' @return RReturns a list of fr (functional redundancy), loss.frac (average fraction of a community required to go extinct prior to trait loss), and express.level (the fraction of a community required to go extinct)
+##' @examples # We should include examples, but I'm kind of confused about how examples work
+
+extinction_simulation <- function(range.express=c(1:100), range.nonexpress=c(0:100), q = 0.5, a.range = seq(0,0.4,by=0.01), loss.thres = 0.99, range.trait = seq(1,1000,by=1), n.community = 100, n.extinct.sim = 10) {#}, ncore = 1) {
+  
+  
+  community.trait <- data.frame(fr=rep(NaN,n.community),
+                                loss.frac=rep(NaN,n.community),
+                                express.level=rep(NaN,n.community))
+  # cl<-parallel::makeCluster(2) #create cluster object
+  # doParallel::registerDoParallel(cores = ncore) #assign number of cores
+  
+  #parallelize extinction simulations
+  
+  # community.trait<-foreach::foreach(i=1:n.community, .combine = rbind) %dopar% {
+  for (i in 1:n.community){ 
+    #define artifical community
+    n.e<-sample(range.express,1) #randomly choose number of trait possesing taxa
+    n.n<-sample(range.nonexpress,1) #randomly choose taxa not possessing trait
+    n.tot=n.e+n.n #calculate community richness
+    S0<-1 
+    R<-c(0:(n.tot-1)) #octaves
+    a<-sample(a.range,1) #choose a parameter for generating lognormal community
+    SR<-S0*exp(-a^2*R^2) #generate abundances of lognormal community
+    relative.abundance<-matrix(SR/sum(SR), nrow = 1) #convert to proportions
+    colnames(relative.abundance) <- 1:n.tot
+    rownames(relative.abundance) <- 1
+    
+    #randomize trait levels among taxa
+    trait.rand<-sample(range.trait,n.e,replace = TRUE) #randomly choose, with replacement, trait levels
+    trait.rand<-matrix(sample(c(trait.rand,rep(0,n.n)),n.tot,replace = FALSE), ncol = 1) #randomize the order of trait levels; random trait vector is concatenated with a 0's vector of length n.n
+    rownames(trait.rand) <- 1:n.tot
+    colnames(trait.rand) <- 1
+    
+    contribution.dist <- t(trait.rand) * relative.abundance
+    contribution.dist <- contribution.dist / sum(contribution.dist)
+    
+    n.member.loss<-rep(NaN,n.extinct.sim) #a dataframe for determining the proportion of a community that goes extinct
+    
+    #perform extinction simulations,it2 replicates 
+    for (k in 1:n.extinct.sim){
+      trait.loss <- 0 #intial trait loss from community aggregated parameter
+      tmp.contribution <- contribution.dist #create dummy data.frame which updates after each iteration
+      member.loss <- 0
+      while(trait.loss < loss.thres) { #keep looping until trait level removed exceeds threshold
+        indx <- sample(1:length(tmp.contribution),1) #randomly select community member
+        trait.loss <- trait.loss + tmp.contribution[indx] #calculate total trait level removed from community aggregated parameter
+        tmp.contribution <- tmp.contribution[-indx] #remove member from community
+        member.loss <- member.loss+1 #monitor number of extinctions
+      }
+      n.member.loss[k] <- member.loss #assign total members loss during replicate to data.frame 
+    }
+    
+    fr<-royalty_fr(relative.abundance,trait.rand,q)[1,1] #caluclate functional redundancy using method here
+    
+    loss.frac=mean(n.member.loss)/n.tot #calculate proportion of community that went extinct
+    community.trait[i,]<-data.frame(fr=fr,
+                                    loss.frac=loss.frac,
+                                    express.level=n.e/n.tot) #create data.frame used for appending replicate simulations together, express level is proportion of community with a trait
+    
+  }
+  
+  # parallel::stopCluster(cl)#stop cluster object
+  return(community.trait)
+}
+#write model outputs
+# write.csv(community.trait,file =filename,quote = FALSE,row.names = FALSE)
+
--- R/royalty_fr.R
+++ R/royalty_fr.R
@@ -2,72 +2,120 @@
 ##'
 ##' @export
 ##' @description Calculates functional redundancy via the Royalty method, as described in INSERT REFERENCE HERE.
-##' @param trait.contribution DESCRIBE THIS
-##' @param q.range DESCRIBE THIS
+##' @param abundance.matrix A traditional taxa abundance matrix. Rows correspond to different samples and columns correspond to different taxa.
+##' @param trait.matrix #A matrix corresponding to traits within taxa. Columns correspond to traits while rows correspond to taxa 
+##' @param q.range The diversity order to evaluate. The default setting evaluates diversity orders 0 through 2.
 ##' @details ANYTHING THAT NEEDS TO GO INTO THE DETAILS SECTION
-##' @return EXPLAIN WHAT IT RETURNS, WITH OBJECT TYPE
+##' @return Returns a list, where individual elements correspond to samples in the abundance matrix. The list includes an estimate of functional redundancy and functional diversity for each diversity order, q, as well as the species richness.
 ##' @examples # We should include examples, but I'm kind of confused about how examples work
+##' abundance.matrix <- read.csv('data/MAG_abundance_table.csv', row.names = 1 ) 
+##' abundance.matrix <- round(abundance.matrix/min(abundance.matrix[abundance.matrix>0])) 
+##' sample.effort <- min(rowSums(abundance.matrix))
+##' abundance.matrix <- vegan::rrarefy(abundance.matrix,sample.effort)
+##' abundance.matrix <- sweep(abundance.matrix,1,rowSums(abundance.matrix),'/')
+##' trait.matrix <- read.csv('data/MAG_enzyme_gene_copies.csv', row.names = 1 )
+##' fr <- royalty_fr(abundance.matrix, trait.matrix, q = 0.5)
+##' fr <- tidyr::separate(fr,sample,into = c("site","size_fraction","depth"), sep = '_')
+##' ggplot2::ggplot(fr,aes(x=trait,y=fr,color=depth))+geom_boxplot()+ylim(0,0.7)  
 ##' @references ADD MANUSCRIPT REFERENCE HERE
 
-royalty_fr <- function(trait.contribution, q.range = seq(0, 2, by = 0.1)) {
-
+royalty_fr <- function(abundance.matrix, trait.matrix, q = 0.5) {
+  
   # ADD SOME CODE TO MAKE SURE THAT PARAMS ARE VALID
-  if(!is.vector(trait.contribution, mode = "numeric")) {
-    stop("The trait.contribution argument must be a numeric vector")
+  if(!all(apply(trait.matrix,1,FUN=is.numeric) == TRUE)) {
+    stop("The trait.matrix argument must be a numeric matrix")
   }
-  if(!is.vector(q.range, mode = "numeric")) {
-    stop("The q.range argument must be a numeric vector")
+  if(!all(apply(abundance.matrix,1,FUN=is.numeric) == TRUE)) {
+    stop("The abundance.matrix argument must be a numeric matrix")
   }
-
-
-  S <- length(trait.contribution)
-    # vanish<-c(0.99999,rep(0.00001/(S-1),S-1))
-    #Equation 3 and 4
-    # trait.contribution<-abundance*trait/sum(abundance*trait)
-    #Equation 6
-    even.community <- rep(sum(trait.contribution) / length(trait.contribution),
-                          length(trait.contribution))
-
-    n.q <- length(q.range)
-    indx <- 1
-
-    fr_out <- data.frame(fr = rep(NaN, n.q),
-                         D = rep(NaN, n.q),
-                         q = rep(NaN, n.q))
-
-    if (sum(even.community) > 0) {
-      for (q in q.range) { # Pretty sure this can be vectorized
-        if (q == 1) {
-          D.q <-
-            exp(-1 * sum(trait.contribution[trait.contribution > 0] * log(trait.contribution[trait.contribution >
-                                                                                               0])))
-          # delta.q=sum(trait.contribution*log(trait.contribution/even.community))
-          # delta.0=sum(vanish*log(vanish/even.community))
-          # fr<-1-delta.q/delta.0
-          # S<-exp(-1*sum(even.community*log(even.community)))
-        } else {
-          D.q <- (sum(trait.contribution[trait.contribution > 0] ^ q)) ^ (1 / (1 -
-                                                                                 q))
-          # D.0<-(sum(vanish^q))^(1/(1-q))
-          # delta.q=S-D.q#1/(q-1)*(D.q^(1-q)-S^(1-q))
-          # delta.0=S-D.0#1/(q-1)*(D.0^(1-q)-S^(1-q))
-          # fr<-1-delta/delta2
-          # fr<-(D.q-1)/(S-1)
-        }
-        fr <- (D.q - 1) / (S - 1)
-        fr_out$q[indx] <- q
-        fr_out$D[indx] <- D.q
-        fr_out$fr[indx] <- fr
-        indx <- indx + 1
+  
+  
+  fr_single_sample <- function(abundance.vector,trait.level, q) {
+    
+    indx <- abundance.vector > 0
+    abundance.vector <- abundance.vector[indx]
+    trait.level <- trait.level[indx]
+    
+    trait.contribution <- abundance.vector * trait.level
+    trait.contribution <- trait.contribution / sum(trait.contribution)
+    
+    S <- length(abundance.vector)
+    
+    fr_sample <- data.frame(fr = NaN,
+                            D = NaN,
+                            q = NaN,
+                            S = NaN)
+    
+    if (!is.na(sum(trait.contribution))) {
+      if (q == 1) {
+        D.q <- exp(-1 * sum(trait.contribution[trait.contribution > 0] * log(trait.contribution[trait.contribution >0])))
+      } else {
+        D.q <- sum(trait.contribution[trait.contribution > 0] ^ q) ^ (1 / (1 - q))
       }
+      
+      fr <- (D.q - 1) / (S - 1)
+      fr_sample$D <- D.q
+      fr_sample$fr <- fr
     } else {
-      fr_out$q[indx] <- q
-      fr_out$D[indx] <- NaN
-      fr_out$fr[indx] <- NaN
+      fr_sample$D <- 0
     }
-
-    return(fr_out)
+    fr_sample$q <- q
+    fr_sample$S <- S
+    
+    return(fr_sample)
   }
+  
+  fr_multiple_sample <- function(abundance.matrix, trait.level, q) {
+    
+    fr <- apply(X = abundance.matrix,
+                MARGIN = 1,
+                FUN = fr_single_sample,
+                trait.level = trait.level,
+                q = q)
+    
+    fr <- cbind(dplyr::bind_rows(fr),
+                sample=names(fr))
+    return(fr)
+  }
+  
+  trait.taxa.names<-rownames(trait.matrix)
+  trait.names<-colnames(trait.matrix)
+  abundance.names <- colnames(abundance.matrix)
+  sample.names <- rownames(abundance.matrix)
+  overlap.names<-intersect(trait.taxa.names,abundance.names)
+  
+  n.taxa <- length(overlap.names)
+  
+  if(n.taxa != length(trait.taxa.names) | n.taxa != length(abundance.names)) {
+    warning('The taxa in the trait matrix and abundance matrix do not match.\nUsing only taxa which appear in both.')
+  }
+  
+  trait.matrix <- trait.matrix[overlap.names,] 
+  trait.matrix <- t(trait.matrix) 
+  colnames(trait.matrix) <- overlap.names
+  rownames(trait.matrix) <- trait.names
+  abundance.matrix <- abundance.matrix[,overlap.names]
+  abundance.matrix <- t(abundance.matrix) #for some reason, R developers decided 1 dimensional matrices should be converted to vectors. Problematic if only analyzing 1 sample. Vectors do not work with apply. Transposing the vector is the current work around
+  rownames(abundance.matrix) <- sample.names
+  
+  fr_out <- apply(X = trait.matrix, 
+                  MARGIN = 1,
+                  FUN = fr_multiple_sample,
+                  abundance.matrix = abundance.matrix,
+                  q = q)
+  
+  fr_out <- lapply(1:length(fr_out),function(x,fr_out,trait.list) {cbind(fr_out[[x]],trait=trait.list[x])},fr_out,names(fr_out))
+  
+  fr_out <- dplyr::bind_rows(fr_out)
+  
+  
+  if(sum(is.na(fr_out$fr))>0) {
+    warning('Some communities do not have any members contributing to a community-aggregated parameter.\nFunctional redundancy is not defined.')
+  }
+  
+  
+  return(fr_out)
+}
 
 
 
